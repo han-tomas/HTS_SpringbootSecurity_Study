f33bd6af7e0f1cbe1aaf0347f318b5e9088e404a
hantaejun <plushan98@naver.com>
2024-01-19 20:51:11 +0900

    로그인 기능 구현

--- src/main/java/com/example/springsecurity/controller/JoinController.java
+++ src/main/java/com/example/springsecurity/controller/JoinController.java
@@ -5,8 +5,10 @@ import com.example.springsecurity.service.JoinService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@ResponseBody
 public class JoinController {
     private final JoinService joinService;
     public JoinController(JoinService joinService){
